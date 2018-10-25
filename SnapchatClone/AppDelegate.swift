4af2c947e78e4ecd6e604aa1085181e97154a192
Kayli Jiang <kaylijiang@gmail.com>
2018-10-24 22:37:59 -0700

    final

---
+++ SnapchatClone/AppDelegate.swift
@@ -0,0 +1,53 @@
+//
+//  AppDelegate.swift
+//  SnapchatClone
+//
+//  Created by Max Miranda on 9/20/18.
+//  Copyright © 2018 ___MaxAMiranda___. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import UserNotifications
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        FirebaseApp.configure()
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        /* PART 1 START */
+        let center =  UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound, .badge]) { (result, error) in
+            //handle result of request failure
+        }
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Come back to the app!"
+        content.body = "You've left the app for more than 5 seconds!"
+        content.sound = UNNotificationSound.default()
+        
+        //notification trigger can be based on time, calendar or location
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval:5.0, repeats: false)
+        
+        //create request to display
+        let request = UNNotificationRequest(identifier: "ContentIdentifier", content: content, trigger: trigger)
+        
+        //add request to notification center
+        center.add(request) { (error) in
+            if error != nil {
+                print("error \(String(describing: error))")
+            }
+        }
+        
+        /* PART 1 FINISH */
+    }
+}
+
